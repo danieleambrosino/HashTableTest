e2efb2fe97c859cef131181853d50eb85431db5b
Daniele Ambrosino <danieleambrosino97@gmail.com>
2017-05-03 18:04:05 +0200

    Implemented collision counter

--- test.py
+++ test.py
@@ -18,11 +18,24 @@ def test_chained(test_input):
     chained_hash_table = hash.ChainedHashTable(hash_table_size)
     hash_table_size = chained_hash_table.size
 
+    collisions = []
+    collisions_dictionary = {}
+
     for percent in percents:
-        for i in xrange(20):
+        for i in xrange(20):  # Insertion tests
             values_to_insert = random_generate(hash_table_size * percent, (0, 100 * hash_table_size))
             for value in values_to_insert:
                 chained_hash_table.insert(value)
+            collisions.append(chained_hash_table.collision_counter)
+
+        # for each percent group, store max, min and average collision number
+        max_collision = max(collisions)
+        min_collision = min(collisions)
+        avg_collision = (max_collision - min_collision) / 2
+        collisions_dictionary[percent] = (max_collision, min_collision, avg_collision)
+        del collisions[:]
+
+    pickle.dump(collisions_dictionary, open("test_chained.pickle", "wb"))
 
 
 def start_tests():
