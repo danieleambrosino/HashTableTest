602f5dc06cc7f898ddc242286fecef51d5d55b1e
Daniele Ambrosino <danieleambrosino97@gmail.com>
2017-05-04 03:01:37 +0200

    Renamed liked list module, minor fixes

---
+++ linked_list.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        item.next = self.head
+        self.head = item
+
+    def is_empty(self):
+        return self.head is None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.next
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.value == item:
+                found = True
+            else:
+                current = current.next
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.value == item:
+                found = True
+            elif current.value is None:
+                return
+            else:
+                previous = current
+                current = current.next
+        if previous is None:
+            self.head = current.next
+        else:
+            previous.next = current.next
