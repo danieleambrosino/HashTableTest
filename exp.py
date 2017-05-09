71e07b285757e371843bde6edc222158a13c558b
Daniele Ambrosino <danieleambrosino97@gmail.com>
2017-05-09 23:11:18 +0200

    Added version restriction, created main() function

--- exp.py
+++ exp.py
@@ -1,68 +1,75 @@
 import pickle
 from test import start_tests
 import matplotlib.pyplot as plt
+from sys import version_info
+
+assert version_info[0] == 2
 
 
-hash_table_size = 1000
-percents = (10, 20, 30, 40, 50, 60, 70, 75, 80, 85, 87, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100)
+def main():
+    hash_table_size = 1000
+    percents = (10, 20, 30, 40, 50, 60, 70, 75, 80, 85, 87, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100)
 
-test_values = (hash_table_size, percents)
+    test_values = (hash_table_size, percents)
 
-pickle.dump(test_values, open("test_input.pickle", "w"))
+    pickle.dump(test_values, open("test_input.pickle", "w"))
 
-start_tests()
+    start_tests()
 
-results = open("test_result.pickle", "r")
-chained_results = pickle.load(results)
-open_results = pickle.load(results)
-open_collisions = open_results[0]
-open_inspections = open_results[1]
+    results = open("test_result.pickle", "r")
+    chained_results = pickle.load(results)
+    open_results = pickle.load(results)
+    open_collisions = open_results[0]
+    open_inspections = open_results[1]
 
-# Chained hash table collisions plot
-x_axis = percents
-y_min = chained_results[:, 0]
-y_avg = chained_results[:, 1]
-y_max = chained_results[:, 2]
+    # Chained hash table collisions plot
+    x_axis = percents
+    y_min = chained_results[:, 0]
+    y_avg = chained_results[:, 1]
+    y_max = chained_results[:, 2]
 
-plt.plot(x_axis, y_min)
-plt.plot(x_axis, y_avg)
-plt.plot(x_axis, y_max)
+    plt.plot(x_axis, y_min)
+    plt.plot(x_axis, y_avg)
+    plt.plot(x_axis, y_max)
 
-plt.xlabel('Load percentage')
-plt.ylabel('Collisions number')
-plt.legend(['Minimum', 'Average', 'Maximum'], loc=2)
-fig = plt.gcf()
-fig.canvas.set_window_title('Chained hash table collisions')
-plt.show()
+    plt.xlabel('Load percentage')
+    plt.ylabel('Collisions number')
+    plt.legend(['Minimum', 'Average', 'Maximum'], loc=2)
+    fig = plt.gcf()
+    fig.canvas.set_window_title('Chained hash table collisions')
+    plt.show()
 
-# Open hash table collisions plot
-y_min = open_collisions[:, 0]
-y_avg = open_collisions[:, 1]
-y_max = open_collisions[:, 2]
+    # Open hash table collisions plot
+    y_min = open_collisions[:, 0]
+    y_avg = open_collisions[:, 1]
+    y_max = open_collisions[:, 2]
 
-plt.plot(x_axis, y_min)
-plt.plot(x_axis, y_avg)
-plt.plot(x_axis, y_max)
+    plt.plot(x_axis, y_min)
+    plt.plot(x_axis, y_avg)
+    plt.plot(x_axis, y_max)
 
-plt.xlabel('Load percentage')
-plt.ylabel('Collisions number')
-plt.legend(['Minimum', 'Average', 'Maximum'], loc=2)
-fig = plt.gcf()
-fig.canvas.set_window_title('Open hash table collisions')
-plt.show()
+    plt.xlabel('Load percentage')
+    plt.ylabel('Collisions number')
+    plt.legend(['Minimum', 'Average', 'Maximum'], loc=2)
+    fig = plt.gcf()
+    fig.canvas.set_window_title('Open hash table collisions')
+    plt.show()
 
-# Open hash table inspection lengths
-y_min = open_inspections[:, 0]
-y_avg = open_inspections[:, 1]
-y_max = open_inspections[:, 2]
+    # Open hash table inspection lengths
+    y_min = open_inspections[:, 0]
+    y_avg = open_inspections[:, 1]
+    y_max = open_inspections[:, 2]
 
-plt.plot(x_axis, y_min)
-plt.plot(x_axis, y_avg)
-plt.plot(x_axis, y_max)
+    plt.plot(x_axis, y_min)
+    plt.plot(x_axis, y_avg)
+    plt.plot(x_axis, y_max)
 
-plt.xlabel('Load percentage')
-plt.ylabel('Inspection sequence length')
-plt.legend(['Minimum', 'Average', 'Maximum'], loc=2)
-fig = plt.gcf()
-fig.canvas.set_window_title('Open hash table inspections length')
-plt.show()
+    plt.xlabel('Load percentage')
+    plt.ylabel('Inspection sequence length')
+    plt.legend(['Minimum', 'Average', 'Maximum'], loc=2)
+    fig = plt.gcf()
+    fig.canvas.set_window_title('Open hash table inspections length')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
